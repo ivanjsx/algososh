c3be49ad0a696617ee831cfddbcf74b10830a0b0
Ivan Shanin <ivanxshanin@gmail.com>
2024-02-05 01:05:47 +0100

    populate app constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,51 @@
-// DELAYS
-export const DELAY_IN_MS = 1000;
-export const SHORT_DELAY_IN_MS = 500;
+export const DEFAULT_ARRAY_SIZE = 5;
+export const DEFAULT_QUEUE_SIZE = 7;
 
-// ELEMENT CAPTIONS
-export const HEAD = "head";
-export const TAIL = "tail";
+export enum Direction {
+  Ascending = "ascending",
+  Descending = "descending",
+};
+
+export enum ElementStates {
+  Default = "default",
+  Selected = "selected",
+  Changing = "changing",
+  Modified = "modified",
+  Invisible = "invisible",
+};
+
+export enum SortingAlgorithms {
+  Bubble = "bubble",
+  Selection = "selection",
+};
+
+export enum ElementCaptions {
+  Top = "top",
+  Head = "head",
+  Tail = "tail",
+};
+
+export enum StackActions {
+  Pop = "pop",
+  Push = "push",
+};
+
+export enum QueueActions {
+  Enqueue = "enqueue",
+  Dequeue = "dequeue",
+};
+
+export enum LinkedListActions {
+  Pop = "pop",
+  Push = "push",
+  Shift = "shift",
+  Insert = "insert",
+  Remove = "remove",
+  Unshift = "unshift",
+};
+
+export enum Delay {
+  None = 0,
+  Short = 500,
+  Medium = 1000,
+};
